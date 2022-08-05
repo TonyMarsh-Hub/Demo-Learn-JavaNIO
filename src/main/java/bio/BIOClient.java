76501131633a8e8e4d672e88127641f0a8dd4d6f
tony <tonymarsh0331@gmail.com>
2022-08-05 19:02:32 +0800

    :beers: 添加BIO server/client 的演示代码

---
+++ src/main/java/bio/BIOClient.java
@@ -0,0 +1,20 @@
+package bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class BIOClient {
+    public static void main(String[] args) {
+        try {
+            //创建客户端
+            Socket client = new Socket("localhost", 10086);
+            String msg = "hi,buddy";
+            OutputStream outputStream = client.getOutputStream();
+            outputStream.write(msg.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
