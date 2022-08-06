abcf1ce2adbd5f790ed852e6aac1c0f22db9372c
tony <tonymarsh0331@gmail.com>
2022-08-06 16:13:09 +0800

    :art: 添加了一段文档解释clear和compact的区别

--- src/main/java/nio/buffer/Demo1.java
+++ src/main/java/nio/buffer/Demo1.java
@@ -30,5 +30,16 @@ public class Demo1 {
 //        buffer.clear();
         //优化3：
         buffer.compact();//也可以使用compact()方法
+
+        /*
+        clear() 将position将被置为0，limit被设置成capacity的值。
+        可以理解为Buffer被标记为清空了，但实际上Buffer中的数据并未清除，只是position和limit的值被重置了。
+        如果Buffer中有一些未读的数据，调用clear()方法，未读数据将“被遗忘”，意味着不再有任何标记会告诉你哪些数据被读过，哪些还没有.
+
+        compact() 将所有未读的数据拷贝到Buffer起始处。然后将position设到最后一个未读元素正后面。
+        limit属性依然像clear()方法一样，设置成capacity。
+        现在Buffer准备好写数据了，但是不会覆盖未读的数据。
+        */
+
     }
 }
