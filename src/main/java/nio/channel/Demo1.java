64fdd14fce22a9213120099c2ce98f32e81d1b31
tony <tonymarsh0331@gmail.com>
2022-08-06 17:34:30 +0800

    :construction: 完成Channel示例代码1，存在待修复的bug → 目标文件中没有实际数据，均为null

---
+++ src/main/java/nio/channel/Demo1.java
@@ -0,0 +1,25 @@
+package nio.channel;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Demo1 {
+    public static void main(String[] args) throws IOException {
+        String msg = "hi,dude";
+        String fileName = "channel01.txt";
+        //创建一个输出流
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        //获取同一个通道--channel的实际类型是FileChannelImpl
+        FileChannel channel = fileOutputStream.getChannel();
+        //创建一个缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        //将信息写入缓冲区中
+        buffer.put(msg.getBytes());
+        //将缓冲区中的数据写到到通道中
+        int num = channel.write(buffer);
+        System.out.println("写入完毕！" + num);
+        fileOutputStream.close();
+    }
+}
